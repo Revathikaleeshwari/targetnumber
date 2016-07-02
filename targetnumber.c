9f8d055faa0596c0a3d884e3c6d9d61a77135424
Revathi N <nrevathibe38@gmail.com>
2016-07-02 14:24:58 +0530

    Create targetnumber.c

---
+++ targetnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch[50]={2,7,11,15};
+int x;
+printf("Enter the target values are:\n");
+scanf("%c",&x);
+str(ch[0]+ch[1]);
+x=ch[0]+ch[1];
+printf("Enter the target: %d",x);
+return 0;
+}
